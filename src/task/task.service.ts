abaf756596d7d0d3ee0163d714ef4736be4ee5b0
Hải Lê <hai.lehoang1501@gmail.com>
2024-01-06 15:22:35 +0700

    update

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -32,23 +32,6 @@ export class TaskService {
       createLightScheduleDto.zoneId,
       createLightScheduleDto,
     );
-    if (this.schedulerRegistry.doesExist('cron', `${zone.id}-light`)) {
-      this.schedulerRegistry.deleteCronJob(`${zone.id}-light`);
-    }
-    const newLightJob = CronJob.from({
-      cronTime: this.getCronTime(zone.lightStartTime),
-      onTick: () => {
-        this.logger.log('turn-on-light');
-        this.client.emit('light', 'on');
-        setTimeout(() => {
-          this.logger.log('turn-off-light');
-          this.client.emit('light', 'off');
-        }, zone.lightTime * 1000);
-      },
-      timeZone: 'system',
-    });
-    this.schedulerRegistry.addCronJob(`${zone.id}-light`, newLightJob);
-    newLightJob.start();
   }
 
   async createIrrigationSchedule(
@@ -58,25 +41,6 @@ export class TaskService {
       createIrrigationScheduleDto.zoneId,
       createIrrigationScheduleDto,
     );
-
-    if (this.schedulerRegistry.doesExist('cron', `${zone.id}-irrigation`)) {
-      this.schedulerRegistry.deleteCronJob(`${zone.id}-irrigation`);
-    }
-
-    const newIrrigationJob = CronJob.from({
-      cronTime: this.getCronTime(zone.irrigationStartTime),
-      onTick: () => {
-        this.client.emit('start-watering', {
-          waterAmount: zone.waterAmount,
-        });
-      },
-      timeZone: 'system',
-    });
-    this.schedulerRegistry.addCronJob(
-      `${zone.id}-irrigation`,
-      newIrrigationJob,
-    );
-    newIrrigationJob.start();
   }
 
   switchLight(deviceMacAddress: string, turn: string) {
